5847684f23225fb46895381bd27a2b42b3ee30f7
来铧敏 <laihuamin@Mac-Pro.local>
2024-07-15 07:42:19 +0800

    feat: 提交guess

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is {}", secret_number);
+
+    println!("please input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("you guess: {}", guess);
 }
