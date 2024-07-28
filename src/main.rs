2b2f80b50d8d1f3ae7fdd64c5fbf3cd34295a33c
来铧敏 <laihuamin@Mac-Pro.local>
2024-07-28 17:43:44 +0800

    feat: 猜数字游戏

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+// std代表标准 io是标准库中的一个库
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
@@ -7,11 +9,34 @@ fn main() {
 
     println!("The secret number is {}", secret_number);
 
-    println!("please input your guess");
+    loop {
+        println!("please input your guess");
+        // let用于生命一个变量 mut代表可变与不可变
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        // io模块调用stdin函数，允许我们处理用户输入
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+        //  rust允许用一个新的值来遮蔽之前的值。
+        // 这里做了一个类型转换，从string转换为一个number类型。
+        // 做处理，如果用户输入非数字，则继续让他猜
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("you guess: {}", guess);
 
-    println!("you guess: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("you win");
+                // 如果猜对了就推出
+                break;
+            }
+        }
+    }
+    
 }
