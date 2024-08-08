9358fb7debd5973e7f9bd47948784b8dcd1ef492
来铧敏 <laihuamin@Mac-Pro.local>
2024-08-08 08:23:30 +0800

    feat: 提交做题

--- src/bin/temperature_transform.rs
+++ src/bin/temperature_transform.rs
@@ -1,44 +1,42 @@
-// 练习题一：在华氏温度和摄氏度之间转换温度。
 use std::io;
+
 fn main() {
-    // 让用户选择是华氏温度还是摄氏温度
-    println!("1代表华氏温度，2代表摄氏温度");
-
-    let mut temperatrue_type = String::new();
-
     loop {
-        println!("请选择你输入的温度格式")
+        println!("1代表华氏温度，2代表摄氏温度");
+        
+        let mut temperatrue_type = String::new();
+        let mut temperature = String::new();
 
+        println!("请选择你输入的温度格式");
         io::stdin().read_line(&mut temperatrue_type).expect("读取失败");
+        temperatrue_type = temperatrue_type.trim().to_string();
+
+        if temperatrue_type != "1" && temperatrue_type != "2" {
+            println!("输入错误，请重新选择温度格式");
+            continue;
+        }
+
+        println!("请输入你的温度值");
+        io::stdin().read_line(&mut temperature).expect("读取失败");
+
+        let temperature: u32 = match temperature.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("输入的不是温度值，请重新开始");
+                continue;
+            }
+        };
+
+        let x = if temperatrue_type == "1" { transform_c_f(temperature) } else { transform_f_c(temperature) };
+        println!("转换结果为 {}", x);
+        break;
     }
-
-    let mut temperature = String::new();
-    // 然后让用户输入温度
-
-    println!("请输入你的温度值");
-
-    io::stdin().read_line(&mut temperature).expect("读取失败");
-
-    // 类型转换，将用户输入的字符串转换成数字
-
-    let temperature: u32 = match temperature.trim().parse() {
-        Ok(num) => num,
-        Err(_) => println!("输入的不是温度值，请重新开始")
-    }
-
-    // 如果是华氏温度就选择华氏温度转换函数
-    let x = if temperatrue_type == '1' {transform_c_f(temperature)} else {transform_f_c(temperature)};
-    // 如果是摄氏温度就选择摄氏温度转换函数
-    println!("transform result is {}", x);
 }
 
-
 fn transform_f_c(temperature: u32) -> u32 {
-    // ℃ = (°F - 32) × 1.8
-    (temperature - 32) * 1.8;
+    (temperature - 32) * 5 / 9
 }
 
 fn transform_c_f(temperature: u32) -> u32 {
-    // f = c / 1.8 + 32
-    temperature / 1.8 + 32;
+    temperature * 9 / 5 + 32
 }
